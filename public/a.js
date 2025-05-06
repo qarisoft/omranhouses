a8a32d47995112a98d29a1e9e54095666d2bc4c8
Salah Al-Salahi <cloud.data.sa@gmail.com>
2025-05-06 13:28:57 +0300

    first commit

---
+++ public/a.js
@@ -0,0 +1,17 @@
+<!-- Snap Pixel Code -->
+// <script type='text/javascript'>
+    (function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+    a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+    r.src=n;var u=t.getElementsByTagName(s)[0];
+    u.parentNode.insertBefore(r,u);})(window,document,
+    'https://sc-static.net/scevent.min.js');
+
+    snaptr('init', 'c7012be4-be1c-4371-b5f8-e7ae10c9fb2e', {
+    'user_email': '__INSERT_USER_EMAIL__'
+});
+
+    snaptr('track', 'PAGE_VIEW');
+
+{/*</script>*/}
+<!-- End Snap Pixel Code -->
